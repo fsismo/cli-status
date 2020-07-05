399459ba48d50edf3837ecb9f9107056ea30ca56
SISMONDA Fernando <fernando@sismonda.com.ar>
2020-07-04 23:42:50 -0300

    Weather extension variables

--- config.bash
+++ config.bash
@@ -7,3 +7,9 @@ BAD='\033[0;31m'
 WAR='\033[1;33m'
 OK='\033[1;32m'
 CONSOLEWITH='60'
+
+# WEATHER EXTENSION
+# I'm gonna use the openweathermap.org api to get the weather data.
+
+OWM_AK=''                   #https://home.openweathermap.org/api_keys
+OWM_CITYID='3435910'        #https://openweathermap.org/city/3435910
