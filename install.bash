b3d8943b67464d5add85ac36095030cf540faff2
SISMONDA Fernando <fernando@sismonda.com.ar>
2020-07-12 21:24:13 -0300

    Bug fixes

--- install.bash
+++ install.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd /opt/status
 ln -s /opt/status/status-initd.bash /etc/init.d/CliStatus
 update-rc.d CliStatus defaults
 chmod +x /etc/init.d/CliStatus
 systemctl enable CliStatus
 systemctl start CliStatus
 systemctl status CliStatus
+cp config.ori.bash config.bash
+echo config.bash > .gitignore
+#git update-index --assume-unchanged config.bash
