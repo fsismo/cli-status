fd1c8fdc1653739f76b0dd9a94e2f190f043db29
SISMONDA Fernando <fernando@sismonda.com.ar>
2020-07-13 00:59:19 -0300

    Better install procedure.

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ update-rc.d CliStatus defaults
 chmod +x /etc/init.d/CliStatus
 systemctl enable CliStatus
 systemctl start CliStatus
-systemctl status CliStatus
 cp config.ori.bash config.bash
 echo config.bash > .gitignore
-#git update-index --assume-unchanged config.bash
+echo -e "First you should set you api key from https://home.openweathermap.org/api_keys and your DNS servers in the config.bash file"
+echo -e "To start the process you should run:\n \033[1;33m  sudo systemctl status CliStatus \033[0m"
