edbb3d81910ef8ce88424d7ee0922378302bc5e6
SISMONDA Fernando <fernando@sismonda.com.ar>
2020-07-16 10:41:29 -0300

    If there is no data dont print info.

--- check-weather.bash
+++ check-weather.bash
@@ -6,6 +6,6 @@ RTN_WEATHER_TEMP=$(echo "$WEATHER" | jq -r '.main.temp')
 RTN_WEATHER_CITY=$(echo "$WEATHER" | jq -r '.name')
 RTN_WEATHER_MAIN=$(echo "$WEATHER" | jq -r '.weather[0].main')
 
-echo "RTN_WEATHER_TEMP=\"$RTN_WEATHER_TEMP C\"" > /dev/shm/STS-WEATHER
+echo "RTN_WEATHER_TEMP=\"$RTN_WEATHER_TEMP\"" > /dev/shm/STS-WEATHER
 echo "RTN_WEATHER_MAIN=\"$RTN_WEATHER_MAIN\"" >> /dev/shm/STS-WEATHER
 echo "RTN_WEATHER_CITY=\"$RTN_WEATHER_CITY\"" >> /dev/shm/STS-WEATHER
